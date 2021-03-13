3cc0cd11c191b3ead1c9a54c316bdc1256ef8214
Patryq <p.kurc@o2.pl>
2021-03-13 13:50:32 +0100

    bubble sorted whith sweep implementation

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,16 +1,16 @@
 def sort(data: list) -> list:
     right_border = len(data)-1
-    while right_border != 0:
-    # for _ in range(len(data) - 1):          # '_' podłoga jako zmienna niepotrzebna
-
-        for i in range(len(data) - 1):
+    sweep = True
+    while sweep:
+        sweep = False
+        for i in range(right_border):
             if data[i] > data[i + 1]:
                 data[i], data[i + 1] = data[i + 1], data[i]
-        right_border -= 1
-        #print(data)
+                sweep = True
+        right_border -=1
     return data
 
 
 if __name__ == "__main__":
     print(sort([5, 4, 3, 2, 1]))
-    #sort([5, 4, 3, 2, 1])
+    # sort([5, 4, 3, 2, 1])
