c2acb843fefd6205762e3314f0ebaa9a03cf0d76
Patryq <p.kurc@o2.pl>
2021-03-13 13:41:42 +0100

    Bubble sort modify implementation (while use)

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,13 +1,16 @@
 def sort(data: list) -> list:
-    for _ in range(len(data) - 1):
+    right_border = len(data)-1
+    while right_border != 0:
+    # for _ in range(len(data) - 1):          # '_' podłoga jako zmienna niepotrzebna
 
         for i in range(len(data) - 1):
             if data[i] > data[i + 1]:
                 data[i], data[i + 1] = data[i + 1], data[i]
-        print(data)
+        right_border -= 1
+        #print(data)
     return data
 
 
 if __name__ == "__main__":
-    # print(sort([5, 4, 3, 2, 1]))
-    sort([5, 4, 3, 2, 1])
+    print(sort([5, 4, 3, 2, 1]))
+    #sort([5, 4, 3, 2, 1])
