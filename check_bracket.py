f2e00f9b48fc9a19c42a6763d0b95c24e60f1f09
Patryq <p.kurc@o2.pl>
2021-03-15 19:46:35 +0100

    Check bracket exercise vol. 1

---
+++ check_bracket.py
@@ -0,0 +1,19 @@
+def validate(sequence: str) -> bool:
+    stack = []
+    for char in sequence:
+        if char not in ["(", ")"]:
+            continue
+        if char == "(":
+            stack.append(char)
+        else:
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    return len(stack) == 0
+
+
+if __name__ == "__main__":
+    assert validate("(())") == True
+    assert validate("())") == False
+    assert validate("))(") == False
